e96c425ed42868485b2b44526ef3c10cac9f6682
Jeff Workman <jworkhollr@aol.com>
2022-02-07 19:01:51 -0500

    Add project files.

---
+++ EducationProject/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EducationProject {
+    class Program {
+        static void Main(string[] args) {
+
+            List<Major> majors = new List<Major>();
+
+            Major business = new Major("Business", 1000);
+            majors.Add(business);
+            Major nursing = new Major("Nursing", 1200);
+            majors.Add(nursing);
+            Major theater = new Major("Theater", 1100);
+            majors.Add(theater);
+            
+            //go thru each item in a collection and put it into major
+            foreach(Major major in majors) {
+                Console.WriteLine($"{major.Id}|{major.Description}|{major.MinSAT}");
+
+            }
+
+            List<Student> students = new List<Student>();
+
+            Student alex = new Student("Alex", "Alex", 1350, 3.4m);
+            alex.Major = alex.GetMajorByDescription("Business", majors);
+            Student drew = new Student("Drew", "Drew", 1350, 3.45m);
+            drew.Major = drew.GetMajorByDescription("Business", majors);
+            Student steven = new Student("Steven", "Steven", 1200, 4.0m);
+
+            students.Add(alex);
+            students.Add(drew);
+            students.Add(steven);
+
+            foreach(Student student in students) {
+                string MajorDescription = student.Major == null
+                    ? "Undecided" : student.Major.Description;
+                Console.WriteLine($"{student.Firstname} {student.Lastname} " +
+                                   $"Major:{MajorDescription}");
+            }
+
+
+        }
+    }
+}
